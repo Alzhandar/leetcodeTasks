aa29648b33a017354e108af9b57cca01833a9f28
Reshord <daribayev1982@bk.ru>
2025-03-26 15:12:09 +0500

    update

---
+++ easy/max-depth-tree.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+s = Solution()
+print(s.maxDepth(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))) == 3) 
+        
